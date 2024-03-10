77685aee0ac48310d05fd5e59d235c7d21ddaec9
hydrotolerance <julzhafiz@gmail.com>
2024-03-10 12:25:28 +0800

    (Add) Adding a Charizard Picture

--- Pokemon/src/components/Header.jsx
+++ Pokemon/src/components/Header.jsx
@@ -4,14 +4,14 @@ import { FaFire, FaBaby } from "react-icons/fa";
 
 export default function Header() {
   let imgs = [
-    'https://external-preview.redd.it/tQged7mKJ3cUpNMq5IMeceZvyKP3cTyHqhNmKEQ0Vv8.png?auto=webp&s=fb5fd61cae0bc9cde2bc2a006b1e2aeb0c935ce9',
+    "https://external-preview.redd.it/tQged7mKJ3cUpNMq5IMeceZvyKP3cTyHqhNmKEQ0Vv8.png?auto=webp&s=fb5fd61cae0bc9cde2bc2a006b1e2aeb0c935ce9",
   ];
   return (
     <div className=" bg-red-500 sticky top-0 z-50">
-              <div className="py-3">
-      <div className="flex justify-between mx-14">
+      <div className="py-3">
+        <div className="flex justify-between mx-14">
           <div>
-            <img src={imgs[0]} className="h-15 w-36"/>
+            <img src={imgs[0]} className="h-15 w-36" />
           </div>
           <ul className="flex items-center justify-center text-white">
             <li className="flex items-center mr-5">
--- Pokemon/src/page/Home.jsx
+++ Pokemon/src/page/Home.jsx
@@ -12,11 +12,22 @@ export default function Home() {
           width={"200px"}
           height={"200px"}
         /> */}
-      <div>
-        <div className="bg-lime-300">
-          <div class="relative w-1/2 bg-gradient-to-r from-white  to-bg-white  h-screen">
-            <div class="absolute left-0 w-1/2 h-full bg-white"></div>
-            <div class="absolute left-0 w-full h-full bg-white -skew-x-12"></div>
+      <Header />
+      <div className="relative">
+        <div className="bg-lime-300 relative">
+          <div className="relative w-1/2 bg-gradient-to-r from-white  to-bg-white  h-screen">
+            <div className="absolute left-0 w-1/2 h-full bg-white"></div>
+            <div className="absolute left-0 w-full h-full bg-white -skew-x-12"></div>
+          </div>
+          <div className="left-1/3 absolute top-1/2 tranform -translate-y-1/2 translate-x-3/4 ">
+            <div>asdfasdf</div>
+          </div>
+          <div className="right-1/3 absolute top-1/2 tranform -translate-y-1/2 translate-x-3/4 ">
+            <div className="flex items-center">
+              <div className=" flex right-0">
+                <img src={charizard} alt="" className="w-[650px] h-[600px]" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
