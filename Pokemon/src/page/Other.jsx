2a308d93b69c8b0c3edb414440287769cbae3ab8
hydrotolerance <julzhafiz@gmail.com>
2024-03-16 22:48:37 +0800

    (Fix) The modal fix

--- Pokemon/src/page/Other.jsx
+++ Pokemon/src/page/Other.jsx
@@ -4,36 +4,79 @@ import { BiUserCircle, BiShow } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Other = ({ post, onClose }) => {
+  const getColorText = (type) => {
+    switch (type) {
+      case "fire":
+        return "bg-orange-500";
+      case "water":
+        return "bg-blue-500";
+      case "grass":
+        return "bg-green-500";
+      case "bug":
+        return "bg-lime-500";
+      case "normal":
+        return "bg-gray-500";
+      case "poison":
+        return "bg-purple-500";
+      case "electric":
+        return "bg-yellow-500";
+      case "ground":
+        return "bg-yellow-700 ";
+      case "psychic":
+        return "bg-pink-500";
+      case "ghost":
+        return "bg-violet-600";
+      case "rock":
+        return "bg-stone-500";
+      case "fighting":
+        return "bg-red-600";
+      case "ice":
+        return "bg-cyan-500";
+      case "flying":
+        return "bg-gray-200 !text-black";
+      case "fairy":
+        return "bg-red-400 text-black";
+      case "steel":
+        return "bg-slate-500";
+      case "dragon":
+        return "bg-indigo-600";
+      case "dark":
+        return "bg-zinc-600";
+    }
+  };
   return (
     <div
       className="fixed top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center bg-black bg-opacity-60"
       onClick={onClose}
     >
       <div
-        className="relative w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 z-50  flex flex-col "
+        className="relative w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 z-50  flex flex-col mx-2"
         onClick={(event) => event.stopPropagation()}
       >
         <AiOutlineClose
           className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
           onClick={onClose}
         />
-        <p>ID: {post.id}</p>
-        <p>Name: {post.name}</p>
-        <p>
-          Type:
-          {post.types[0].type.name.charAt(0).toUpperCase() +
-            post.types[0].type.name.slice(1)}
-        </p>
-        <p>
-          Abilities:
-          {post.abilities[0].ability.name}
-          {post.abilities[1].ability.name}
-        </p>
         <img
           src={post.sprites.other.dream_world.front_default}
           alt=""
-          className="w-20 h-20"
+          className="w-40 h-40 mx-auto"
         />
+        <p className="text-center text-2xl">
+          {post.name.charAt(0).toUpperCase() + post.name.slice(1)}
+        </p>
+        <p
+          className={`mx-auto text-center w-1/4 rounded mb-3 text-white ${getColorText(
+            post.types[0].type.name
+          )}`}
+        >
+          {post.types[0].type.name.charAt(0).toUpperCase() +
+            post.types[0].type.name.slice(1)}
+        </p>
+        <div className="grid mx-auto md:grid-cols-2">
+          <p className="text-center px-5">Weight: {post.weight}</p>
+          <p className="text-center px-5">Height: {post.height}</p>
+        </div>
       </div>
     </div>
   );
