c0097588c1f649f79e177fe3495bacd564dfe6af
hydrotolerance <julzhafiz@gmail.com>
2024-04-17 16:14:17 +0800

    (Fix) the color on hover container

--- Pokemon/src/page/pokemondex/Gen1.jsx
+++ Pokemon/src/page/pokemondex/Gen1.jsx
@@ -80,39 +80,39 @@ export default function Gen1({ post }) {
   const getColorBackground = (type) => {
     switch (type) {
       case "fire":
-        return "bg-orange-300 hover:bg-orange-500";
+        return "bg-orange-300 hover:bg-orange-400 ";
       case "water":
-        return "bg-blue-300";
+        return "bg-blue-200 hover:bg-blue-400";
       case "grass":
-        return "bg-green-300";
+        return "bg-green-300 hover:bg-green-400";
       case "bug":
-        return "bg-lime-300";
+        return "bg-lime-300 hover:bg-lime-400";
       case "normal":
-        return "bg-gray-300";
+        return "bg-gray-300 hover:bg-gray-400";
       case "poison":
-        return "bg-purple-300";
+        return "bg-purple-300 hover:bg-purple-400";
       case "electric":
-        return "bg-yellow-300";
+        return "bg-yellow-300 hover:bg-yellow-400";
       case "ground":
-        return "bg-yellow-600";
+        return "bg-yellow-500 hover:bg-yellow-700";
       case "psychic":
-        return "bg-pink-300";
+        return "bg-pink-300 hover:bg-pink-400";
       case "ghost":
-        return "bg-violet-400";
+        return "bg-violet-400 hover:bg-violet-500";
       case "rock":
-        return "bg-stone-400";
+        return "bg-stone-400 hover:bg-stone-500";
       case "fairy":
-        return "bg-red-200 text-black";
+        return "bg-red-200 text-black hover:bg-red-300";
       case "ice":
-        return "bg-cyan-200";
+        return "bg-cyan-200 hover:bg-cyan-300";
       case "steel":
-        return "bg-slate-400";
+        return "bg-slate-400 hover:bg-slate-500";
       case "dragon":
-        return "bg-indigo-400";
+        return "bg-indigo-400 hover:bg-indigo-500";
       case "dark":
-        return "bg-zinc-500 text-white";
+        return "bg-zinc-500 text-white hover:bg-zinc-600";
       case "fighting":
-        return "bg-red-400 text-white";
+        return "bg-red-400 text-white hover:bg-red-500";
     }
   };
   const openModal = (post) => {
@@ -141,7 +141,7 @@ export default function Gen1({ post }) {
   const getColorText = (type) => {
     switch (type) {
       case "fire":
-        return "bg-orange-500";
+        return "bg-orange-500 ";
       case "water":
         return "bg-blue-500";
       case "grass":
@@ -193,9 +193,9 @@ export default function Gen1({ post }) {
             <p className="text-center my-3 ml-12 text-xl">Loading ...</p>
           </div>
         ) : (
-          <div className="p-4 md:ml-64">
-            <div className="flex justify-end mb-8">
-              <div className="hover:border-red-500 relative mx-3">
+          <div className="p-4 md:ml-64 max-sm:w-[450px]">
+            <div className="lg:flex justify-end mb-8">
+              <div className="hover:border-red-500 relative me-3">
                 <select
                   value={postDropdown}
                   onChange={hanldeDropdown}
@@ -235,7 +235,7 @@ export default function Gen1({ post }) {
                   icon={
                     <IoSearch
                       size={"28"}
-                      className=" w-8 h-8 absolute ml-72 flex bottom-10 top-4 right-3 justify-end items-end text-gray-400 pointer-events-none"
+                      className=" w-8 h-8 absolute ml-72 flex bottom-10 top-4 lg:right-3 justify-end items-end text-gray-400 pointer-events-none"
                     />
                   }
                 />
@@ -254,11 +254,11 @@ export default function Gen1({ post }) {
               </div>
             )}
             {filteredPokemon.length > 0 && (
-              <div class="grid xl:grid-cols-5 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 gap-10">
+              <div class="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 gap-x-10">
                 {filteredPokemon.map((post, index) => (
                   <div
                     key={index}
-                    className={`border  text-center rounded-lg relative cursor-pointer  my-5 shadow-lg ${getColorBackground(
+                    className={`border  text-center rounded-lg relative cursor-pointer  my-5 shadow-lg transition-colors duration-500 ${getColorBackground(
                       post.types[0].type.name
                     )} `}
                     onClick={() => openModal(post)}
@@ -266,17 +266,14 @@ export default function Gen1({ post }) {
                     <img
                       src={post.sprites.other.home.front_default}
                       alt=""
-                      className="h-48 w-48 absolute mx-auto bottom-32 right-0 left-0 z-10"
+                      className="h-48 w-48 absolute mx-auto bottom-32 right-0 left-0 z-10 mb-3"
                     />
                     <div className="relative py-7 overflow-hidden">
                       <CgPokemon className="h-80 w-80 absolute opacity-20 text-white mx-auto top-52 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-                      <div dir="rtl"></div>
-                      <div className="pt-24 pb-3 z-10">
-                        <p className="text-xl font-medium mb-2">
-                          •
+                      <div className="pt-28 pb-3 z-10">
+                        <p className="text-xl font-bold mb-2 ">
                           {post.name.charAt(0).toUpperCase() +
                             post.name.slice(1)}
-                          •
                         </p>
                         <p className="">#{("00" + post.id).slice(-3)}</p>
                       </div>
