3b181a686bd4c857e4309f08249cdd83dc1d3570
hydrotolerance <julzhafiz@gmail.com>
2024-04-27 21:31:25 +0800

    (Fix) Fix the Health

--- Pokemon/src/page/Other1.jsx
+++ Pokemon/src/page/Other1.jsx
@@ -74,50 +74,45 @@ function Other1() {
     <>
       {posts ? (
         <div>
-                    <h2 className="text-center text-4xl text-gray-500">
+          <h2 className="text-center text-4xl text-gray-500">
             {posts.name.toUpperCase()}
           </h2>
-          <div className="flex justify-between items-center">
-          <table className="text-end">
-            <tr>
-              <td><span className="text-gray-500 mb-1">WEIGHT</span></td>
-              <td className=""><span> {(Math.round(posts.weight * 2.20462262185 * 100) / 1000).toFixed(1)}lbs ( {(parseInt(posts.weight) / 10).toFixed(1)} kg) </span></td>
-            </tr>
-            <tr>
-              <td> <span className="text-gray-500  mb-1">HEIGHT</span> </td>
-              <td> <span> {parseFloat((posts.height * 3.28) / 10).toFixed(1).slice(0, 1) +`'` + parseFloat((posts.height * 3.28) / 10) .toFixed(1) .slice(2)}{" "} Feet ({posts.height / 10} m) </span> </td>
-            </tr>
-            <tr>
-              <td> <span className="text-gray-500  mb-1">ABILITIES</span> </td>
-              <td> <span> {posts.abilities.map((ability, index) => ( <span key={index}> {ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1)} {index !== posts.abilities.length - 1 && ", "} </span> ))} </span> </td>
-            </tr>
-            <tr>
-              <td> <span className="text-gray-500  mb-1">BASE EXPERIECE</span> </td>
-              <td> <span>{posts.base_experience}</span> </td>
-            </tr>
-            <tr>
-              <td> <span className="text-gray-500  mb-1">FORMS</span> </td>
-              <td> <span>{posts.name.charAt(0).toUpperCase() + posts.name.slice(1)}</span> </td>
-            </tr>
-          </table>
-
-          <div>
+          <div className="grid md:grid-cols-3 text-sm">
+            <div className="m-auto">
+              <table>
+              <tr className="">
+                <td className="pb-3"><span className="text-gray-500">WEIGHT</span></td>
+                <td className="ps-10 pb-3 text-xs whitespace-nowrap"><span> {(Math.round(posts.weight * 2.20462262185 * 100) / 1000).toFixed(1)}lbs ( {(parseInt(posts.weight) / 10).toFixed(1)} kg) </span></td>
+              </tr>
+              <tr>
+                <td className="pb-3"> <span className="text-gray-500">HEIGHT</span> </td>
+                <td className="ps-10 pb-3  text-xs whitespace-nowrap"> <span> {parseFloat((posts.height * 3.28) / 10).toFixed(1).slice(0, 1) +`'` + parseFloat((posts.height * 3.28) / 10) .toFixed(1) .slice(2)}{" "} Feet ({posts.height / 10} m) </span> </td>
+              </tr>
+              <tr>
+                <td className="pb-3"> <span className="text-gray-500  mb-1">ABILITIES</span> </td>
+                <td className="ps-10 pb-3 text-xs whitespace-nowrap"> <span> {posts.abilities.map((ability, index) => ( <span key={index}> {ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1)} {index !== posts.abilities.length - 1 && ", "} </span> ))} </span> </td>
+              </tr>
+              <tr>
+                <td className="pb-3 text-xs"> <span className="text-gray-500  mb-1">BASE EXP</span> </td>
+                <td className="ps-10 pb-3 text-xs whitespace-nowrap"> <span>{posts.base_experience}</span> </td>
+              </tr>
+              <tr>
+                <td className="pb-3"> <span className="text-gray-500  mb-1">FORMS</span> </td>
+                <td className="ps-10 pb-3 text-xs whitespace-nowrap"> <span>{posts.name.charAt(0).toUpperCase() + posts.name.slice(1)}</span> </td>
+              </tr>
+            </table>
+          </div>
+          <div className="mx-auto">
             <img
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${posts.id}.png`}
-              className="mx-auto"
+              
             />
           </div>
-        
-          <div>
-            <div className="grid md:grid-cols-2">
-              <div className=" text-gray-500">
-                <p>HEALTH:</p>
-              </div>
-              <div class=" max-md:w-full bg-gray-200 rounded-full h-3.5 dark:bg-gray-700">
-                <div
-                  className={`bg-green-600 h-3.5 rounded-full relative`}
-                  style={{ width: `${Measurement(posts.stats[0].base_stat)}%` }}
-                >
+          <div className="my-auto w-auto mx-4">
+            <div className="">
+              <p>HEALTH</p>
+              <div class=" max-md:w-full bg-gray-200 rounded h-3.5 dark:bg-gray-700">
+                <div className={`bg-green-600 h-3.5 rounded relative`} style={{ width: `${Measurement(posts.stats[0].base_stat)}%` }}>
                   <div className="text-xs text-white absolute right-0 pe-2 top-[-1px]">
                     {" "}
                     {posts.stats[0].base_stat}{" "}
@@ -125,15 +120,10 @@ function Other1() {
                 </div>
               </div>
             </div>
-            <div className="grid md:grid-cols-2">
-              <div className="me-3 text-gray-500">
-                <p>ATTACK:</p>
-              </div>
-              <div class=" max-md:w-full bg-gray-200 rounded-full h-3.5 dark:bg-gray-700">
-                <div
-                  className={`bg-red-600 h-3.5 rounded-full relative`}
-                  style={{ width: `${Measurement(posts.stats[1].base_stat)}%` }}
-                >
+            <div>
+              <p>HEALTH</p>
+              <div class=" max-md:w-full bg-gray-200 rounded h-3.5 dark:bg-gray-700">
+                <div className={`bg-green-600 h-3.5 rounded relative`} style={{ width: `${Measurement(posts.stats[1].base_stat)}%` }}>
                   <div className="text-xs text-white absolute right-0 pe-2 top-[-1px]">
                     {" "}
                     {posts.stats[1].base_stat}{" "}
@@ -141,15 +131,10 @@ function Other1() {
                 </div>
               </div>
             </div>
-            <div className="grid md:grid-cols-2">
-              <div className="me-3 text-gray-500">
-                <p>SPECIAL ATTACK:</p>
-              </div>
-              <div class=" max-md:w-full bg-gray-200 rounded-full h-3.5 dark:bg-gray-700">
-                <div
-                  className={`bg-red-600 h-3.5 rounded-full relative`}
-                  style={{ width: `${Measurement(posts.stats[2].base_stat)}%` }}
-                >
+            <div>
+              <p>HEALTH</p>
+              <div class=" max-md:w-full bg-gray-200 rounded h-3.5 dark:bg-gray-700">
+                <div className={`bg-green-600 h-3.5 rounded relative`} style={{ width: `${Measurement(posts.stats[2].base_stat)}%` }}>
                   <div className="text-xs text-white absolute right-0 pe-2 top-[-1px]">
                     {" "}
                     {posts.stats[2].base_stat}{" "}
@@ -157,15 +142,11 @@ function Other1() {
                 </div>
               </div>
             </div>
-            <div className="grid md:grid-cols-2">
-              <div className="me-3 text-gray-500">
-                <p>SPECIAL DEFENSE:</p>
-              </div>
-              <div class=" max-md:w-full bg-gray-200 rounded-full h-3.5 dark:bg-gray-700">
-                <div
-                  className={`bg-blue-600 h-3.5 rounded-full relative`}
-                  style={{ width: `${Measurement(posts.stats[3].base_stat)}%` }}
-                >
+
+            <div>
+              <p>HEALTH</p>
+              <div class=" max-md:w-full bg-gray-200 rounded h-3.5 dark:bg-gray-700">
+                <div className={`bg-green-600 h-3.5 rounded relative`} style={{ width: `${Measurement(posts.stats[3].base_stat)}%` }}>
                   <div className="text-xs text-white absolute right-0 pe-2 top-[-1px]">
                     {" "}
                     {posts.stats[3].base_stat}{" "}
@@ -173,15 +154,11 @@ function Other1() {
                 </div>
               </div>
             </div>
-            <div className="grid md:grid-cols-2">
-              <div className="me-3 text-gray-500">
-                <p>SPEED:</p>
-              </div>
-              <div class=" max-md:w-full bg-gray-200 rounded-full h-3.5 dark:bg-gray-700">
-                <div
-                  className={`bg-blue-600 h-3.5 rounded-full relative`}
-                  style={{ width: `${Measurement(posts.stats[4].base_stat)}%` }}
-                >
+
+            <div>
+            <span>HEALTH</span>
+              <div class=" max-md:w-full bg-gray-200 rounded h-3.5 dark:bg-gray-700">
+                <div className={`bg-green-600 h-3.5 rounded relative`} style={{ width: `${Measurement(posts.stats[4].base_stat)}%` }}>
                   <div className="text-xs text-white absolute right-0 pe-2 top-[-1px]">
                     {" "}
                     {posts.stats[4].base_stat}{" "}
@@ -189,7 +166,9 @@ function Other1() {
                 </div>
               </div>
             </div>
+
           </div>
+          
           <div>
           </div>
             
